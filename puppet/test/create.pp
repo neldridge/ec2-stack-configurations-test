9be5b61548273e7f519bd746aad440d6548fd41d
Nick Eldridge <nicholas.eldridge@gmail.com>
2016-03-06 16:43:46 -0500

    Updated with base staging configurations.

--- puppet/test/create.pp
+++ puppet/test/create.pp
@@ -8,20 +8,6 @@ Ec2_instance {
   availability_zone => 'us-east-1d',
 }
 
-Elb_loadbalancer {
-  region => 'us-east-1',
-}
-
-ec2_securitygroup { 'lb-sg':
-  ensure      => present,
-  description => 'Security group for load balancer',
-  ingress     => [{
-    protocol => 'tcp',
-    port     => 80,
-    cidr     => '0.0.0.0/0'
-  }],
-}
-
 ec2_securitygroup { 'web-sg':
   ensure      => present,
   description => 'Security group for web servers',
@@ -34,19 +20,8 @@ ec2_securitygroup { 'web-sg':
   }],
 }
 
-ec2_securitygroup { 'db-sg':
-  ensure      => present,
-  description => 'Security group for database servers',
-  ingress     => [{
-    security_group => 'web-sg',
-  },{
-    protocol => 'tcp',
-    port     => 22,
-    cidr     => '0.0.0.0/0'
-  }],
-}
 
-ec2_instance { ['web-1', 'web-2']:
+ec2_instance { 'web-1':
   ensure          => present,
   image_id        => 'ami-8fcee4e5', 
   security_groups => ['web-sg'],
@@ -58,33 +33,3 @@ ec2_instance { ['web-1', 'web-2']:
   }
 }
 
-ec2_instance { 'db-1':
-  ensure          => present,
-  image_id        => 'ami-8fcee4e5', 
-  security_groups => ['db-sg'],
-  instance_type   => 't2.micro',
-  monitoring      => true,
-  tags            => {
-    department => 'engineering',
-    project    => 'cloud',
-    created_by => $::id,
-  },
-  block_devices => [
-    {
-      device_name => '/dev/sda1',
-      volume_size => 8,
-    }
-  ]
-}
-
-elb_loadbalancer { 'lb-1':
-  ensure             => present,
-  availability_zones => ['us-east-1d'],
-  instances          => ['web-1', 'web-2'],
-  listeners          => [{
-    protocol           => 'tcp',
-    load_balancer_port => 80,
-    instance_protocol  => 'tcp',
-    instance_port      => 80,
-  }],
-}
